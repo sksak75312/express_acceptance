5f8da938c1938f91d6b59f5945c5de7684676579
sksak75312 <sksak75312@gmail.com>
2022-10-11 22:40:41 +0800

    Initial project

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const exphbs = require('express-handlebars')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/:pages', (req, res) => {
+  const page = req.params.pages.charAt(0).toUpperCase() + req.params.pages.slice(1)
+  res.render('pages', {pages : page})
+})
+
+app.listen(port, () => {
+  console.log(`Express running localhost:${port}`)
+})
+
